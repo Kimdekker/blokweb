48676ad47803656ead6bde14b90c43a3f5f7a903
Kim Dekker <kim11dekekr@gmail.com>
2023-09-14 17:06:50 +0200

    lots of styling and some script

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,19 @@
 // JavaScript Document
 console.log("hi");
+
+var menuBtn = document.querySelector('header button');
+
+menuBtn.onclick = foldMenu
+
+function foldMenu(){
+  var menuBtn = document.querySelector('header nav');
+  var lineOne = document.querySelector('header button span:nth-of-type(1)');
+  var lineTwo = document.querySelector('header button span:nth-of-type(2)');
+  var lineThree = document.querySelector('header button span:nth-of-type(3)');
+
+  menuBtn.classList.toggle('showmenu');
+  lineOne.classList.toggle('rotateplus');
+  lineTwo.classList.toggle('scaley');
+  lineThree.classList.toggle('rotatemin');
+}
+
