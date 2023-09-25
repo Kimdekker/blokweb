0d6d46251410146c19b33b6f4328f3a796cea17f
Kim Dekker <kim11dekekr@gmail.com>
2023-09-25 14:01:56 +0200

    try'ing scroll

--- scripts/script.js
+++ scripts/script.js
@@ -17,3 +17,25 @@ function foldMenu(){
   lineThree.classList.toggle('rotatemin');
 }
 
+
+// bron: https://codepen.io/alvarotrigo/pen/XWaYRKK
+
+function reveal() {
+  var reveals = document.querySelector("main section:nth-of-type(4)");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
+
+
