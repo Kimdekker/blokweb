77e01836129e4c4afe9f2f7d71c2b7aee5f70a44
Kim Dekker <kim11dekekr@gmail.com>
2023-09-26 14:01:14 +0200

    sections basis finished

--- scripts/script.js
+++ scripts/script.js
@@ -18,24 +18,61 @@ function foldMenu(){
 }
 
 
-// bron: https://codepen.io/alvarotrigo/pen/XWaYRKK
+// // bron: https://codepen.io/alvarotrigo/pen/XWaYRKK
 
-function reveal() {
-  var reveals = document.querySelector("main section:nth-of-type(4)");
+// function reveal() {
+//   var reveals = document.querySelector("main section:nth-of-type(4)");
 
-  for (var i = 0; i < reveals.length; i++) {
-    var windowHeight = window.innerHeight;
-    var elementTop = reveals[i].getBoundingClientRect().top;
-    var elementVisible = 150;
+//   for (var i = 0; i < reveals.length; i++) {
+//     var windowHeight = window.innerHeight;
+//     var elementTop = reveals[i].getBoundingClientRect().top;
+//     var elementVisible = 150;
 
-    if (elementTop < windowHeight - elementVisible) {
-      reveals[i].classList.add("active");
-    } else {
-      reveals[i].classList.remove("active");
-    }
-  }
-}
+//     if (elementTop < windowHeight - elementVisible) {
+//       reveals[i].classList.add("active");
+//     } else {
+//       reveals[i].classList.remove("active");
+//     }
+//   }
+// }
 
-window.addEventListener("scroll", reveal);
+// window.addEventListener("scroll", reveal);
 
 
+
+
+// document.addEventListener('DOMContentLoaded', function() {
+//   const radios = document.querySelectorAll('input[name="latest_awards"]');
+//   const rows = document.querySelectorAll('.award, .honor');
+
+//   radios.forEach(radio => {
+//     radio.addEventListener('change', function() {
+//       rows.forEach(row => row.classList.remove('selected'));
+//       document.querySelectorAll('.' + this.value).forEach(row => row.classList.add('selected'));
+//     });
+//   });
+// });
+
+
+// $(document).ready(function() {
+//   $('input[name="latest_awards"]').change(function() {
+//     var value = $(this).val();
+//     if (value == 'award') {
+//       $('.honor').addClass('hidden');
+//       $('.award').removeClass('hidden');
+//     } else if (value == 'honor') {
+//       $('.award').addClass('hidden');
+//       $('.honor').removeClass('hidden');
+//     }
+//   });
+// });
+
+
+
+// bron: https://jsfiddle.net/ydchauh/4cbk0sq5/
+
+var cursor = document.getElementById("cursor");
+document.body.addEventListener("mousemove", function(e) {
+  cursor.style.left = e.clientX + "px",
+    cursor.style.top = e.clientY + "px";
+});
