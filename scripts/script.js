48a986f2766bd16da56e05b712a6244753c01dbe
Kim Dekker <kim11dekekr@gmail.com>
2023-09-27 15:39:58 +0200

    we need to push

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,3 @@
-// JavaScript Document
 console.log("hi");
 
 var menuBtn = document.querySelector('header button');
@@ -16,56 +15,6 @@ function foldMenu(){
   lineTwo.classList.toggle('leave');
   lineThree.classList.toggle('rotatemin');
 }
-
-
-// // bron: https://codepen.io/alvarotrigo/pen/XWaYRKK
-
-// function reveal() {
-//   var reveals = document.querySelector("main section:nth-of-type(4)");
-
-//   for (var i = 0; i < reveals.length; i++) {
-//     var windowHeight = window.innerHeight;
-//     var elementTop = reveals[i].getBoundingClientRect().top;
-//     var elementVisible = 150;
-
-//     if (elementTop < windowHeight - elementVisible) {
-//       reveals[i].classList.add("active");
-//     } else {
-//       reveals[i].classList.remove("active");
-//     }
-//   }
-// }
-
-// window.addEventListener("scroll", reveal);
-
-
-
-
-// document.addEventListener('DOMContentLoaded', function() {
-//   const radios = document.querySelectorAll('input[name="latest_awards"]');
-//   const rows = document.querySelectorAll('.award, .honor');
-
-//   radios.forEach(radio => {
-//     radio.addEventListener('change', function() {
-//       rows.forEach(row => row.classList.remove('selected'));
-//       document.querySelectorAll('.' + this.value).forEach(row => row.classList.add('selected'));
-//     });
-//   });
-// });
-
-
-// $(document).ready(function() {
-//   $('input[name="latest_awards"]').change(function() {
-//     var value = $(this).val();
-//     if (value == 'award') {
-//       $('.honor').addClass('hidden');
-//       $('.award').removeClass('hidden');
-//     } else if (value == 'honor') {
-//       $('.award').addClass('hidden');
-//       $('.honor').removeClass('hidden');
-//     }
-//   });
-// });
 
 
 
